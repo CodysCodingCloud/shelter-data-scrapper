1bf22a7707f49e704d0c644a9d6168bf4a799fdc
cody <adwingsx@aim.com>
2023-05-16 11:51:10 -0400

    data part1

---
+++ savefile2.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+export default function saveint(links, filename) {
+	// creates folder if it does not exist
+	if (!fs.existsSync("scrappedContent")) {
+		fs.mkdirSync("scrappedContent");
+	}
+	// writes the files
+	fs.writeFile(
+		`scrappedContent/${filename}.js`,
+		JSON.stringify(links),
+		(err) => {
+			if (err) throw err;
+			console.log(filename, "saved!");
+		}
+	);
+}
--- scrapper.js
+++ scrapper.js
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer";
 import save from "./saveToFile.js";
-
+import saveint from "./savefile2.js";
 export default async function puppeteerScrapper(url) {
 	// converts url into a useable format for the save function later
 	if (typeof url === "string") {
@@ -17,14 +17,11 @@ export default async function puppeteerScrapper(url) {
 			document.querySelectorAll("table > tbody > tr > td:first-child > a")
 		).map((e) => e.href)
 	);
+	saveint(citylinks, "citylinks");
 	const shelterlinks = [];
-	let count = 0;
 	for (let city of citylinks) {
-		// let city = citylinks[0];
-
-		// console.log(typeof city);
-		if (count < 3) {
-			console.log("goin to:", city);
+		// console.log("goin to:", city);
+		try {
 			await page.goto(new URL(city));
 			const shelterlink = await page.evaluate(() =>
 				Array.from(
@@ -36,13 +33,54 @@ export default async function puppeteerScrapper(url) {
 			for (let link of shelterlink) {
 				shelterlinks.push(link);
 			}
+		} catch (error) {
+			console.log("skipped city:", city);
 		}
-		count++;
 	}
-	console.log(count);
+	saveint(shelterlinks, "shelterlinks");
+	let shelterDataList = [];
+	for (let shelter of shelterlinks) {
+		try {
+			await page.goto(new URL(shelter));
+			const shelterData = {};
+
+			let addressdata = await page.evaluate(() =>
+				document
+					.querySelector("div.entry_content > div.row > div.col.col_4_of_12")
+					.innerText.split("\n")
+			);
+			shelterData.addressLine1 = addressdata[1];
+			shelterData.city = addressdata[2].split(", ")[0];
+			shelterData.state = "NY";
+			shelterData.stateAbbreviation = "New York";
+			let postal = addressdata[2].match(/\d{5}/);
+			shelterData.postal = postal ? postal[0] : "00000";
+			shelterData.name = await page.evaluate(
+				() =>
+					document.querySelector("div.entry_content > h1.entry_title").innerText
+			);
+			shelterData.organization = shelterData.name;
+
+			let [phone, website] = await page.evaluate(() => {
+				let contact = document.querySelectorAll(
+					"div.entry_content > div.row > div.col.col_8_of_12 > p > a"
+				);
+				return [contact[0].innerText.trim(), contact[1].href];
+			});
+			shelterData.phone = phone;
+			shelterData.website = website;
+			shelterData.description = await page.evaluate(
+				() => document.querySelector("div.entry_content > p").innerText
+			);
+			shelterDataList.push(shelterData);
+		} catch (error) {
+			console.log("skipped shelter:", shelter);
+		}
+	}
+
 	// closes browser
 	await browser.close();
-	let data = shelterlinks;
+	let data = shelterDataList;
 	data.__proto__.save = save;
 	data.__proto__.url = url;
 	return data;
