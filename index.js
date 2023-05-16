6aec29effe09da4657f37b07eb4f188a43a318e2
cody <adwingsx@aim.com>
2023-05-16 15:59:49 -0400

    update filter

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import puppeteerScrapper from "./scrapper.js";
 const testLink = "https://www.homelessshelterdirectory.org/state/new-york.html";
 const scrappedLinksPuppeteer = await puppeteerScrapper(testLink);
 console.log("scrapping ny data");
-// console.log(scrappedLinksPuppeteer);
-scrappedLinksPuppeteer.save();
+console.log(scrappedLinksPuppeteer);
+// scrappedLinksPuppeteer.save();
--- scrapper.js
+++ scrapper.js
@@ -1,6 +1,9 @@
 import puppeteer from "puppeteer";
 import save from "./saveToFile.js";
 import saveint from "./savefile2.js";
+// import { cities as citylinks } from "./skippedlinks.js";
+// import { shelter as shelterlinks } from "./skippedlinks.js";
+
 export default async function puppeteerScrapper(url) {
 	// converts url into a useable format for the save function later
 	if (typeof url === "string") {
@@ -13,74 +16,88 @@ export default async function puppeteerScrapper(url) {
 
 	// Grabs HTML content
 	const citylinks = await page.evaluate(() =>
-		Array.from(
-			document.querySelectorAll("table > tbody > tr > td:first-child > a")
-		).map((e) => e.href)
+		Array.from(document.querySelectorAll("table > tbody > tr"))
+			.filter((row) => {
+				const secondColumn = row.querySelector("td:nth-child(2)");
+				if (!secondColumn) return false;
+				return secondColumn.innerText.trim();
+			})
+			.map((row) => {
+				console.log("hi?");
+				return row.querySelector("td:first-child > a").href;
+			})
 	);
-	saveint(citylinks, "citylinks");
-	const shelterlinks = [];
-	for (let city of citylinks) {
-		// console.log("goin to:", city);
-		try {
-			await page.goto(new URL(city));
-			const shelterlink = await page.evaluate(() =>
-				Array.from(
-					document.querySelectorAll(
-						"div.layout_post_2.clearfix > div.item_content > a.btn.btn_red"
-					)
-				).map((e) => e.href)
-			);
-			for (let link of shelterlink) {
-				shelterlinks.push(link);
-			}
-		} catch (error) {
-			console.log("skipped city:", city);
-		}
-	}
-	saveint(shelterlinks, "shelterlinks");
-	let shelterDataList = [];
-	for (let shelter of shelterlinks) {
-		try {
-			await page.goto(new URL(shelter));
-			const shelterData = {};
+	// saveint(citylinks, "citylinks");
 
-			let addressdata = await page.evaluate(() =>
-				document
-					.querySelector("div.entry_content > div.row > div.col.col_4_of_12")
-					.innerText.split("\n")
-			);
-			shelterData.addressLine1 = addressdata[1];
-			shelterData.city = addressdata[2].split(", ")[0];
-			shelterData.state = "NY";
-			shelterData.stateAbbreviation = "New York";
-			let postal = addressdata[2].match(/\d{5}/);
-			shelterData.postal = postal ? postal[0] : "00000";
-			shelterData.name = await page.evaluate(
-				() =>
-					document.querySelector("div.entry_content > h1.entry_title").innerText
-			);
-			shelterData.organization = shelterData.name;
+	// // visits cities and grabs shelter links
+	// const shelterlinks = [];
+	// for (let city of citylinks) {
+	// 	try {
+	// 		await page.goto(new URL(city));
+	// 		const shelterlink = await page.evaluate(() =>
+	// 			Array.from(
+	// 				document.querySelectorAll(
+	// 					"div.layout_post_2.clearfix > div.item_content > a.btn.btn_red"
+	// 				)
+	// 			).map((e) => e.href)
+	// 		);
+	// 		for (let link of shelterlink) {
+	// 			shelterlinks.push(link);
+	// 		}
+	// 	} catch (error) {
+	// 		console.log("skipped city:", city);
+	// 		console.log(error);
+	// 	}
+	// }
+	// // saveint(shelterlinks, "shelterlinks");
 
-			let [phone, website] = await page.evaluate(() => {
-				let contact = document.querySelectorAll(
-					"div.entry_content > div.row > div.col.col_8_of_12 > p > a"
-				);
-				return [contact[0].innerText.trim(), contact[1].href];
-			});
-			shelterData.phone = phone;
-			shelterData.website = website;
-			shelterData.description = await page.evaluate(
-				() => document.querySelector("div.entry_content > p").innerText
-			);
-			shelterDataList.push(shelterData);
-		} catch (error) {
-			console.log("skipped shelter:", shelter);
-		}
-	}
+	// // visits shelters and grabs shelter data
+	// let shelterDataList = [];
+	// for (let shelter of shelterlinks) {
+	// 	try {
+	// 		await page.goto(new URL(shelter));
+	// 		const shelterData = {};
+
+	// 		let addressdata = await page.evaluate(() =>
+	// 			document
+	// 				.querySelector("div.entry_content > div.row > div.col.col_4_of_12")
+	// 				.innerText.split("\n")
+	// 		);
+	// 		shelterData.addressLine1 = addressdata[1];
+	// 		shelterData.city = addressdata[2].split(", ")[0];
+	// 		shelterData.state = "New York";
+	// 		shelterData.stateAbbreviation = "NY";
+	// 		let postal = addressdata[2].match(/\d{5}/);
+	// 		shelterData.postal = postal ? postal[0] : "00000";
+	// 		shelterData.name = await page.evaluate(
+	// 			() =>
+	// 				document.querySelector("div.entry_content > h1.entry_title").innerText
+	// 		);
+	// 		shelterData.organization = shelterData.name;
+
+	// 		let [phone, website] = await page.evaluate(() => {
+	// 			let contact = document.querySelectorAll(
+	// 				"div.entry_content > div.row > div.col.col_8_of_12 > p > a"
+	// 			);
+	// 			let phone = contact[0]?.innerText?.trim() || "";
+	// 			let website = contact[1]?.href || "";
+	// 			return [phone, website];
+	// 		});
+	// 		shelterData.phone = phone;
+	// 		shelterData.website = website;
+	// 		shelterData.description = await page.evaluate(
+	// 			() => document.querySelector("div.entry_content > p").innerText
+	// 		);
+	// 		shelterDataList.push(shelterData);
+	// 	} catch (error) {
+	// 		console.log("skipped shelter:", shelter);
+	// 		console.log(error);
+	// 	}
+	// }
 
 	// closes browser
 	await browser.close();
-	let data = shelterDataList;
+	let data = citylinks;
 	data.__proto__.save = save;
 	data.__proto__.url = url;
 	return data;
