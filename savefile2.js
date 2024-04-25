ef25bc1a333959fae9943076d523129bf6822273
Cody_UB <codylchan@gmail.com>
2024-04-25 18:40:06 -0400

    update save to file mod

--- savefile2.js
+++ savefile2.js
@@ -1,16 +1,29 @@
 import fs from "fs";
-export default function saveint(links, filename) {
-	// creates folder if it does not exist
-	if (!fs.existsSync("scrappedContent")) {
-		fs.mkdirSync("scrappedContent");
-	}
-	// writes the files
-	fs.writeFile(
-		`scrappedContent/${filename}.json`,
-		JSON.stringify(links),
-		(err) => {
-			if (err) throw err;
-			console.log(filename, "saved!");
-		}
-	);
+export default function saveToFile(
+    content,
+    filename,
+    relativePathOfFolder = "scrappedContent",
+    fileType = "json"
+) {
+    // creates folder if it does not exist
+    if (!fs.existsSync(relativePathOfFolder)) {
+        fs.mkdirSync(relativePathOfFolder);
+    }
+    // writes the files
+    let data;
+    if (fileType.lower() === "json") {
+        data = JSON.stringify(content);
+    } else {
+        data = content;
+    }
+    fs.writeFile(
+        `${relativePathOfFolder}/${filename}.${fileType}`,
+        data,
+        (err) => {
+            if (err) throw err;
+            console.log(
+                `data saved to ${relativePathOfFolder}/${filename}.${fileType}`
+            );
+        }
+    );
 }
